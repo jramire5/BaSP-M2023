25013d3b6279618ed3b98f7c2977d04957c8b856
jramire5 <jjnicoramirez@gmail.com>
2023-04-15 17:26:21 -0300

    BaSP-W05-2023: variables and operators completed

---
+++ Week-05/01-variables-and-operators.js
@@ -0,0 +1,15 @@
+// a
+var number1 = 4;
+var number2 = 5;
+var addNumbers = number1+number2;
+console.log("1a. if you add " + number1 + " and  " + number2 + " the result is " + addNumbers);
+// b
+var word1 = "Hola";
+var word2 = "Como estás";
+var addWords = word1 + word2;
+console.log("1b. " + addWords)
+// c
+var word3 = "Hello"
+var word4 = "How are you"
+var countWords = word3.length + word4.length
+console.log("1c. " + countWords)
